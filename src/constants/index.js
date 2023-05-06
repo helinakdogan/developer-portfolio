9e62350cf5c0460f734ff14296559e07b8919360
Helin Akdoğan <akdoganhelin@gmail.com>
2023-05-06 04:53:33 +0300

    06.05.2023

--- src/constants/index.js
+++ src/constants/index.js
@@ -123,7 +123,6 @@ import {
   const experiences = [
     
     {
-      //"#383E56"
       title: "College student",
       company_name: "Bahcesehir University",
       icon: bau,
@@ -150,32 +149,7 @@ import {
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
     },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },*/
+   */
   ];
   
   const testimonials = [
