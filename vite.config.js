e1b97a72f94e614e428fdb23e09853f4bf65098e
Helin Akdoğan <akdoganhelin@gmail.com>
2023-05-06 04:07:34 +0300

    06.05.2023

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //plugins: [react()],
-  base: 'https://helinakdogan.github.io/'
+  plugins: [react()],
+  base: '/helinakdogan.github.io/'
 })
 
